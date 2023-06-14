5acfce587fc40b12f7ce735230189e46400215f5
3lswear <me@email.com>
2023-06-14 17:01:52 +0300

    make it not not work

--- ,clean_for_nop.sh
+++ ,clean_for_nop.sh
@@ -1,6 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 echo "DELETING EXTRA FILES NOW, CONFIRM?"
 read
-fd --type f --extension txt --extension su --extension marks -X rm -v 
-fd --type f CHANGELOG -X rm -v
-fd --type f README.md -X rm -v
+fdfind --type f --extension txt --extension su --extension marks --extension sh -X rm -v
+fdfind --type f CHANGELOG -X rm -v
+fdfind --type f README.md -X rm -v
+fdfind --type empty -X rm -rfv
