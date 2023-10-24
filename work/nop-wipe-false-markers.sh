e70e66134bea149d61eb474594b7201112fa2ebe
3lswear <me@email.com>
2023-10-24 15:58:34 +0300

    nop-wipe-false-markers: improve matching

--- work/nop-wipe-false-markers.sh
+++ work/nop-wipe-false-markers.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
-grep -ar --include '*.lib' --include '*.obj' \
-	$'\xba\xba\xad\xde\x01\x02\x04\x80' --only-matching --byte-offset |
-	tee | #optional
-	cut -d':' -f 1,2 --output-delimiter=' ' |
+
+grep -ar --include '*.lib' --include '*.obj' --include '*.pch' \
+	$'\xba\xba\xad\xde\x01\x02\x04\x80' --only-matching --byte-offset | \
+	tee | #optional \
+	cut -d':' -f 1,2 | \
+	sed "s/^/'/;s/:/' /" | \
 	xargs -L 1 bash -xc 'printf "ZZZZZZZZ" | dd of="$0" bs=1 seek="$1" conv=notrunc'
