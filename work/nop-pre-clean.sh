3894f2fde18233189dd51d38c9d695e998b9b1d7
3lswear <me@email.com>
2023-10-24 15:58:11 +0300

    added more types, new method to delete

--- work/nop-pre-clean.sh
+++ work/nop-pre-clean.sh
@@ -2,21 +2,25 @@
 echo "DELETING EXTRA FILES NOW, CONFIRM?"
 read
 echo "> by extension:"
-fdfind --type f --extension txt --extension xml -X rm -v
+fdfind -uu --type f --extension txt --extension xml -X rm -v
 echo "> text files:"
-fdfind --type f --glob CHANGELOG -X rm -v
-fdfind --type f --glob README.md -X rm -v
+fdfind -uu --type f --glob CHANGELOG -X rm -v
+fdfind -uu --type f --glob README.md -X rm -v
 echo "> empty:"
-fdfind --type empty --type f -X rm -rfv
+fdfind -uu --type empty --type f -X rm -rfv
 
 echo "> ascii text"
-fdfind --type=f -x file {} | grep -F ': ASCII text, with no line terminators' | cut -d':' -f 1 | xargs -n1 rm -v
+# fdfind -uu --type=f -x file "{}" | grep -F ': ASCII text, with no line terminators' | cut -d':' -f 1 | sed "s/^/'/;s/$/'/" | xargs -n1 rm -v
+fdfind -uu --type=f -x bash -c 'file -b "$1" | grep -q "ASCII text" && rm -v "$1"' bash "{}"
 
 echo "> just ascii text"
-fdfind --type=f -x file {} | grep ': ASCII text$' | cut -d':' -f 1 | xargs -n1 rm -v
+# fdfind -uu --type=f -x file "{}"| grep ': ASCII text$' | cut -d':' -f 1 | sed "s/^/'/;s/$/'/" | xargs -n1 rm -v
 
 echo "> very short file"
-fdfind --type=f -x file {} | grep ': very short file (no magic)$' | cut -d':' -f 1 | xargs -n1 rm -v
+fdfind -uu --type=f -x file "{}" | grep ': very short file (no magic)$' | cut -d':' -f 1 | sed "s/^/'/;s/$/'/" | xargs -n1 rm -v
 
 echo "> Bourne-Again shell scripts"
-fdfind --type=f -x file {} | grep -F ': Bourne-Again shell script' | cut -d':' -f 1 | xargs -n1 rm -v
+fdfind -uu --type=f -x file "{}" | grep -F ': Bourne-Again shell script' | cut -d':' -f 1 | sed "s/^/'/;s/$/'/" | xargs -n1 rm -v
+
+echo "> Unicode text"
+fdfind -uu --type=f -x file "{}" | grep -F ': Unicode text' | cut -d':' -f 1 | sed "s/^/'/;s/$/'/" | xargs -n1 rm -v
