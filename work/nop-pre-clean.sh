c5c3909ec577395b7a7285a471972f82528a294e
3lswear <me@email.com>
2023-11-16 10:41:45 +0300

    add remove_by_signature func

--- work/nop-pre-clean.sh
+++ work/nop-pre-clean.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
+
+remove_by_signature() {
+	fdfind -uu --type=f -x bash -c "file -b \"\$1\" | grep -q \"$@\" && rm -v \"\$1\"" bash "{}"
+}
+
 echo "DELETING EXTRA FILES NOW, CONFIRM?"
 read
+
+echo "> files with unprintable filenames"
+LC_ALL=C find . -type f -name '*[! -~]*' -exec rm -v {} \;
+
 echo "> by extension:"
 fdfind -uu --type f --extension txt --extension xml -X rm -v
+
 echo "> text files:"
 fdfind -uu --type f --glob CHANGELOG -X rm -v
 fdfind -uu --type f --glob README.md -X rm -v
+
 echo "> empty:"
 fdfind -uu --type empty --type f -X rm -rfv
 
 echo "> ascii text"
-# fdfind -uu --type=f -x file "{}" | grep -F ': ASCII text, with no line terminators' | cut -d':' -f 1 | sed "s/^/'/;s/$/'/" | xargs -n1 rm -v
-fdfind -uu --type=f -x bash -c 'file -b "$1" | grep -q "ASCII text" && rm -v "$1"' bash "{}"
-
-# echo "> just ascii text"
-# fdfind -uu --type=f -x file "{}"| grep ': ASCII text$' | cut -d':' -f 1 | sed "s/^/'/;s/$/'/" | xargs -n1 rm -v
+# fdfind -uu --type=f -x bash -c 'file -b "$1" | grep -q "ASCII text" && rm -v "$1"' bash "{}"
+remove_by_signature "ASCII text"
 
 echo "> very short file"
 fdfind -uu --type=f -x bash -c 'file -b "$1" | grep -q "very short file (no magic)" && rm -v "$1"' bash "{}"
@@ -24,3 +32,6 @@ fdfind -uu --type=f -x bash -c 'file -b "$1" | grep -q "Bourne-Again shell scrip
 
 echo "> Unicode text"
 fdfind -uu --type=f -x bash -c 'file -b "$1" | grep -q "Unicode text" && rm -v "$1"' bash "{}"
+
+echo "> MS Windows shortcut"
+remove_by_signature "MS Windows shortcut"
