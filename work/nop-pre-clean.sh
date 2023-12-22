e3891a29b32127ec71e912d97f13b4dc67750add
3lswear <me@email.com>
2023-12-22 16:16:32 +0300

    change to sh & remove symlinks

--- work/nop-pre-clean.sh
+++ work/nop-pre-clean.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 remove_by_signature() {
-	fdfind -uu --type=f -x bash -c "file -b \"\$1\" | grep -q \"$@\" && rm -v \"\$1\"" bash "{}"
+	fdfind -uu --type=f -x sh -c "file -b \"\$1\" | grep -q \"$@\" && rm -v \"\$1\"" sh "{}"
 }
 
 echo "DELETING EXTRA FILES NOW, CONFIRM?"
@@ -10,8 +10,11 @@ read
 echo "> files with unprintable filenames"
 LC_ALL=C find . -type f -name '*[! -~]*' -exec rm -v {} \;
 
+echo "> symlinks:"
+fdfind -uu --type=symlink -x rm -v
+
 echo "> by extension:"
-fdfind -uu --type f --extension txt --extension xml -X rm -v
+fdfind -uu --type f --extension txt --extension xml -x rm -v
 
 echo "> text files:"
 fdfind -uu --type f --glob CHANGELOG -X rm -v
@@ -25,13 +28,13 @@ echo "> ascii text"
 remove_by_signature "ASCII text"
 
 echo "> very short file"
-fdfind -uu --type=f -x bash -c 'file -b "$1" | grep -q "very short file (no magic)" && rm -v "$1"' bash "{}"
+fdfind -uu --type=f -x sh -c 'file -b "$1" | grep -q "very short file (no magic)" && rm -v "$1"' sh "{}"
 
 echo "> Bourne-Again shell scripts"
-fdfind -uu --type=f -x bash -c 'file -b "$1" | grep -q "Bourne-Again shell script" && rm -v "$1"' bash "{}"
+fdfind -uu --type=f -x sh -c 'file -b "$1" | grep -q "Bourne-Again shell script" && rm -v "$1"' sh "{}"
 
 echo "> Unicode text"
-fdfind -uu --type=f -x bash -c 'file -b "$1" | grep -q "Unicode text" && rm -v "$1"' bash "{}"
+fdfind -uu --type=f -x sh -c 'file -b "$1" | grep -q "Unicode text" && rm -v "$1"' sh "{}"
 
 echo "> MS Windows shortcut"
 remove_by_signature "MS Windows shortcut"
